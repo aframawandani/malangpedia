7467e9e4c6dcf738b9aa2bccb09c3b15c63f8fb8
= <=>
2021-01-13 18:00:33 +0700

    fixed missing 'Auth' class

--- app/Http/Requests/Auth/PostLoginRequest.php
+++ app/Http/Requests/Auth/PostLoginRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class PostLoginRequest extends FormRequest
